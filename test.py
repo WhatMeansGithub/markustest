584b9dbe375110a7c32d8ab9a025e2ec150d14e9
whatmeansgithub <drinkyoghurt@gmail.com>
2024-12-03 11:02:09 +0100

    I forgot how to git

---
+++ test.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import os
+port = int(os.environ.get("PORT"))
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hey there what's up world we just chillin"
+
+if  __name__ == "main":
+    app.run(debug=True, host="0.0.0.0", port=port)
+    
