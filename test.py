a8acebb72f8eb5a9e2aa74de303d775f2a575083
whatmeansgithub <drinkyoghurt@gmail.com>
2024-12-03 14:05:24 +0100

    fixed

--- test.py
+++ test.py
@@ -8,6 +8,6 @@ app = Flask(__name__)
 def home():
     return "Hey there what's up world we just chillin"
 
-if  __name__ == "main":
+if  __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=port)
     
